9cb8b47c6660a1287bcb40102239c974eb31636d
webpero <per.olav@mariussen.net>
2017-10-17 08:58:35 +0200

    MVC-version

---
+++ bildeM.js
@@ -0,0 +1,46 @@
+/* Håndtering av søk på bilder (Model)
+ * 16.10.2017 Per Olav Mariussen
+ *
+ *******************************/
+
+var bildeModel = function bildeModel() {
+};
+
+/* Utfør søk på Google med *query*, hent ut treff nr *index*, og kjør callback *fn* for behandling av data */
+bildeModel.prototype.hentBilde = function hentBilde(query, index, fn) {
+	$.ajax({
+		url: 'https://www.googleapis.com/customsearch/v1',
+		dataType: "jsonp",
+		data: {	
+			q: query,											// Søkestrengen
+			cx: '018034702328520342012:y80oci2ue2i',			// CSE: webpero-peroma 
+			/*key: 'AIzaSyAL8Of35Vjc2CeUAbSPXc1zd1ugUmYL4Q',	// Google API-key for github&heroku */
+			key: 'AIzaSyCUb7lLbMRJkweAbcXiS3ejObHqnlDkKOQ',		// Google API-key for test				
+			num: 10
+		},
+		success: function(response) {
+			if ( response.error !== undefined ) {
+				// HTTP-response er 200, men det har oppstått feil
+				console.log("Error: "+(response.error !== undefined ? response.error.errors[0].reason : "Ukjent feil!") );
+			}
+			else {
+				let bilde = {};
+				if( response.searchInformation !== undefined && response.searchInformation.totalResults > 0 && response.items[index] !== undefined ) {
+					/* Tilordne riktige returdata (basert på ønsket indeks) fra søk til et bilde-objekt */
+					bilde.navn = response.items[index].title;
+					bilde.info = response.items[index].snippet;
+					bilde.url = (response.items[index].pagemap !== undefined && response.items[index].pagemap.cse_image !== undefined ? response.items[index].pagemap.cse_image[0].src : "");
+					bilde.index = index;
+					bilde.count = response.items.length;
+					
+				}
+				/* Kjør Callback function med bilde-objektet som data (tomt objekt hvis ingen resultater) */
+				fn(bilde);  
+			}
+		},
+		error: function(resonse) {
+			// HTTP-response er 4XX
+			console.log("Error: "+(response.error !== undefined ? response.error.errors[0].reason : "Ukjent feil!") );
+		}
+	});
+};
---
+++ bildeV.js
@@ -0,0 +1,38 @@
+﻿/* Håndtering av visning av bilder (View)
+ * 16.10.2017 Per Olav Mariussen
+ *
+ *******************************/
+ 
+var bildeView = function bildeView(element) {
+	this.element = element;
+	this.onClick = null;
+};
+
+/* Vis bilde med overskrift og info + hådtering av knapper for forrige/neste */
+bildeView.prototype.render = function render(viewModel) {
+	let prevBilde, nextBilde;
+	if ( viewModel.navn !== undefined && viewModel.info !== undefined && viewModel.url !== undefined ) {
+		$(this.element).html( 
+			'<p>' +
+			'<button id="prevBilde" href="javascript:void(0);" data-bilde-index="' + viewModel.previousIndex + '">Forrige</button> ' +
+			'<button id="nextBilde" href="javascript:void(0);" data-bilde-index="' + viewModel.nextIndex + '">Neste</button>' +
+			'</p>' +
+			'<h3>' + viewModel.navn + '</h3>' +
+			'<p>' + viewModel.info + '</p>' +
+			'<p><img id="image" src="' + viewModel.url + '" alt="' + viewModel.navn + '" /></p>'
+		);
+		this.previousIndex = viewModel.previousIndex;
+		this.nextIndex = viewModel.nextIndex;
+
+		$('#prevBilde').click(this.onClick);
+		$('#nextBilde').click(this.onClick);
+		$('#nextBilde').focus();
+	} else {
+		$(this.element).html( '<img id="image" src="no-results.gif" />' );
+	}
+};
+
+/* Vis indikator på at søk utføres/data hentes (vent) */
+bildeView.prototype.loading = function loading() {
+	$(this.element).html( '<img id="image" src="loading.gif" />' );
+};
